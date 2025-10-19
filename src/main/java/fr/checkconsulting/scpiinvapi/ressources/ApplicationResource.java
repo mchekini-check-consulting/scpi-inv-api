05f0a29eba2f1532014375e988c9873eb72e0c5f
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-10-19 04:11:58 +0200

    Set helm chart for api

---
+++ src/main/java/fr/checkconsulting/scpiinvapi/ressources/ApplicationResource.java
@@ -0,0 +1,26 @@
+package fr.checkconsulting.scpiinvapi.ressources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1")
+public class ApplicationResource {
+
+    @Value("${app.name:DemoApplication}")
+    private String appName;
+
+    @Value("${app.version:1.0.0}")
+    private String appVersion;
+
+    @Value("${app.message:hello from backend}")
+    private String appMessage;
+
+    @GetMapping("/info")
+    public String getApplicationInfo() {
+        return String.format(
+                "Application: %s | Version: %s | Message: %s",
+                appName, appVersion, appMessage
+        );
+    }
+}
