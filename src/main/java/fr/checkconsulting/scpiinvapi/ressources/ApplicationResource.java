ac18ab52f3e5afc617f1e8a500807ed8ff8fb22f
mchekini <me.chekini@gmail.com>
2025-10-19 15:36:23 +0200

    fix the path of application informations

--- src/main/java/fr/checkconsulting/scpiinvapi/ressources/ApplicationResource.java
+++ src/main/java/fr/checkconsulting/scpiinvapi/ressources/ApplicationResource.java
@@ -2,9 +2,11 @@ package fr.checkconsulting.scpiinvapi.ressources;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController
+@RequestMapping("api/v1")
 public class ApplicationResource {
 
     @Value("${app.name:DemoApplication}")
@@ -16,7 +18,7 @@ public class ApplicationResource {
     @Value("${app.message:hello from backend}")
     private String appMessage;
 
-    @GetMapping("/info")
+    @GetMapping("info")
     public String getApplicationInfo() {
         return String.format(
                 "Application: %s | Version: %s | Message: %s",
