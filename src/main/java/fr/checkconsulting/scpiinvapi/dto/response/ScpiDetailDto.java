4786a9e9435b71810324d971d8e6e21684b0c7fb
Meziani Belkacem <meziani.belkacem@corepulse.fr>
2025-11-04 02:20:49 +0100

    add id to scpi detail dto

--- src/main/java/fr/checkconsulting/scpiinvapi/dto/response/ScpiDetailDto.java
+++ src/main/java/fr/checkconsulting/scpiinvapi/dto/response/ScpiDetailDto.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class ScpiDetailDto {
-
+    private Long id; 
     private String name;
     private String manager;
     private BigDecimal capitalization;
