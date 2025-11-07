e281256b8d931ded949538ea5bf9dcb6b4663828
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-11-08 00:39:37 +0100

    prepare kafka brokers to acced by multi-env kafka topic by env

---
+++ src/main/java/fr/checkconsulting/scpiinvapi/Config/KafkaTopicConfig.java
@@ -0,0 +1,24 @@
+package fr.checkconsulting.scpiinvapi.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    private final TopicNameProvider topicNameProvider;
+
+    public KafkaTopicConfig(TopicNameProvider topicNameProvider) {
+        this.topicNameProvider = topicNameProvider;
+    }
+
+    @Bean
+    public NewTopic documentValidationTopic() {
+        return TopicBuilder.name(topicNameProvider.getDocumentValidationTopic())
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+}
---
+++ src/main/java/fr/checkconsulting/scpiinvapi/service/KafkaProducerService.java
@@ -0,0 +1,26 @@
+package fr.checkconsulting.scpiinvapi.service;
+
+import fr.checkconsulting.scpiinvapi.config.TopicNameProvider;
+import fr.checkconsulting.scpiinvapi.dto.request.UserDocumentDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final TopicNameProvider topicNameProvider;
+
+    public void sendDocumentEvent(UserDocumentDto documentDto) {
+        String topic = topicNameProvider.getDocumentValidationTopic();
+        log.info(" Envoi du message Kafka sur le topic [{}] pour l'utilisateur [{}], document [{}]",
+                topic, documentDto.getUserEmail(), documentDto.getStoredFileName());
+
+        kafkaTemplate.send(topic, documentDto);
+    }
+
+}
+
