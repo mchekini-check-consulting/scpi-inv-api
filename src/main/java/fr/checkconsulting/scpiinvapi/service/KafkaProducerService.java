4cb68870d984a08ff9dafa44c9af0b7301c9cc49
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-11-08 07:10:25 +0100

    prepare kafka multi-env within one broker to deploy

--- src/main/java/fr/checkconsulting/scpiinvapi/service/KafkaProducerService.java
+++ src/main/java/fr/checkconsulting/scpiinvapi/service/KafkaProducerService.java
@@ -1,24 +1,25 @@
 package fr.checkconsulting.scpiinvapi.service;
 
+import fr.checkconsulting.scpiinvapi.config.TopicNameProvider;
 import fr.checkconsulting.scpiinvapi.dto.request.UserDocumentDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import static fr.checkconsulting.scpiinvapi.utils.Constants.DOCUMENT_VALIDATION_TOPIC;
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaProducerService {
-
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final TopicNameProvider topicNameProvider;
+
     public void sendDocumentEvent(UserDocumentDto documentDto) {
+        String topic = topicNameProvider.getDocumentValidationTopic();
+        log.info(" Envoi du message Kafka sur le topic [{}] pour l'utilisateur [{}], document [{}]",
+                topic, documentDto.getUserEmail(), documentDto.getStoredFileName());
 
-        log.info(" Envoi du message Kafka pour l'utilisateur [{}], document [{}]",
-                documentDto.getUserEmail(), documentDto.getStoredFileName());
-
-        kafkaTemplate.send(DOCUMENT_VALIDATION_TOPIC, documentDto);
-
+        kafkaTemplate.send(topic, documentDto);
     }
+
 }
