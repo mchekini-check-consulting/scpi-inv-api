b8166be0c053e1a2c49ae901fe34519170455529
Meziani Belkacem <belkacem.meziani@corepulse.dz>
2025-11-29 15:25:28 +0100

    add investment duration months

--- src/main/java/fr/checkconsulting/scpiinvapi/mapper/InvestmentMapper.java
+++ src/main/java/fr/checkconsulting/scpiinvapi/mapper/InvestmentMapper.java
@@ -12,6 +12,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -28,6 +29,7 @@ public interface InvestmentMapper {
     @Mapping(source = "scpi.sectors", target = "scpiType", qualifiedByName = "mapSectorsToMainType")
     @Mapping(source = "scpi", target = "distributionRate", qualifiedByName = "extractRate")
     @Mapping(source = "scpi.manager", target = "scpiManagerName")
+    @Mapping(source = "investmentDate", target = "investmentDurationMonths", qualifiedByName = "calculateDuration")
     InvestmentResponseDto toResponseDTO(Investment entity);
 
     List<InvestmentResponseDto> toResponseDTOList(List<Investment> entities);
@@ -54,5 +56,15 @@ public interface InvestmentMapper {
                 .max(java.util.Comparator.comparing(DistributionRate::getDistributionYear))
                 .map(DistributionRate::getRate)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    @Named("calculateDuration")
+    default Long calculateDuration(LocalDateTime investmentDate) {
+        if (investmentDate == null) {
+            return 0L;
+        }
+        
+        LocalDateTime now = LocalDateTime.now();
+        return java.time.temporal.ChronoUnit.MONTHS.between(investmentDate, now);
     }
 }
